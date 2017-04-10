e59ff6ad309f5c469da25ef01bc05e2fa38e8335
nwre87 <nwre87@gmail.com>
2017-04-09 15:07:16 -1000

    add webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,27 @@
+const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  devtool: 'cheap-eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    './src/index',
+  ],
   output: {
-    path: path.resolve('dist'),
+    path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
+    publicPath: '/static/',
   },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
-    ],
-  },
+      {
+        test: /\.js$/,
+        loaders: ['react-hot', 'babel'],
+        include: path.join(__dirname, 'src'),
+      }
+    ]
+  }
 };
