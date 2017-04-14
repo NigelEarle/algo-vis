80a7adc80913e051f67308925eb2c62d250400b5
nwre87 <nwre87@gmail.com>
2017-04-13 17:13:34 -1000

    change bubble sort - add localStorage

--- server/controllers/bubbleSort.js
+++ server/controllers/bubbleSort.js
@@ -1,28 +1,37 @@
+const redis = require('redis');
 module.exports = (req, res) => {
-  console.log('hitt');
+  console.log('bubble sort hitt');
   const array = [12, 34, 82, 98, 76, 53, 1, 49, 22, 61, 5];
-  let iterNum = 0;
-  let idx = 0;
+  let swapped = true;
 
-  res.setSseHeaders(); // set event/stream headers
-
-  (function iteration() {
-    if (idx >= array.length - iterNum - 1) {
-      idx = 0;
-      iterNum += 1;
-    }
-    if (iterNum < array.length) {
-      if (array[idx] > array[idx + 1]) {
-        const temp = array[idx];
-        array[idx] = array[idx + 1];
-        array[idx + 1] = temp;
-        res.sseSend(array);
+  while (swapped) {
+    swapped = false;
+    for (let i = 0; i < array.length; i += 1) {
+      if (array[i] > array[i + 1]) {
+        const temp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
+        swapped = true;
       }
-      idx += 1;
-      setTimeout(iteration, 500);
-    } else {
-      res.end();
     }
-  }());
+  }
 
+  // (function iteration() {
+  //   if (idx >= array.length - iterNum - 1) {
+  //     idx = 0;
+  //     iterNum += 1;
+  //   }
+  //   if (iterNum < array.length) {
+  //     if (array[idx] > array[idx + 1]) {
+  //       const temp = array[idx];
+  //       array[idx] = array[idx + 1];
+  //       array[idx + 1] = temp;
+  //       res.sseSend(array);
+  //     }
+  //     idx += 1;
+  //     setTimeout(iteration, 500);
+  //   } else {
+  //     res.end();
+  //   }
+  // }());
 };
