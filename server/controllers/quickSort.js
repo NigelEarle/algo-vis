c28c912db361950601d969e3419fef3b155bed2f
nwre87 <nwre87@gmail.com>
2017-06-24 17:01:42 -1000

    attempt to do quicksort sequentially without recursion

--- server/controllers/quickSort.js
+++ server/controllers/quickSort.js
@@ -1,3 +1,24 @@
-module.exports = () => {
-  return 'quickSort';
+const timerRes = require('../utils/timer');
+
+module.exports = (req, res) => {
+  const array = [12, 34, 82, 98, 76, 53, 1, 49, 22, 61, 5];
+
+  const pivot = array[0];
+  const left = [];
+  const right = [];
+
+  let partition = true;
+
+  const interimResult = [];
+  do {
+    partition = false;
+    for (let i = 0; i < array.length; i += 1) {
+      if (array[i] <= pivot) {
+        left.push(array[i]);
+      } else {
+        right.push(array[i]);
+      }
+    }
+    interimResult.push(left.concat(right));
+  } while (partition);
 };
--- src/App.js
+++ src/App.js
@@ -2,5 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { HomeComponent } from './components';
-
 ReactDOM.render(<HomeComponent />, document.querySelector('#root'));
